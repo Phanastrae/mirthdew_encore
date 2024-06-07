f087e32d727577d6784645d74f9098f1f7735225
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2024-06-07 01:24:58 +0100

    Project Setup

---
+++ src/client/java/phanastrae/mirthlight_encore/MirthlightEncoreClient.java
@@ -0,0 +1,10 @@
+package phanastrae.mirthlight_encore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MirthlightEncoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/phanastrae/mirthlight_encore/MirthlightEncoreDataGenerator.java
@@ -0,0 +1,11 @@
+package phanastrae.mirthlight_encore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MirthlightEncoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
