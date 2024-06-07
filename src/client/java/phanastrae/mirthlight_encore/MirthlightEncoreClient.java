5ba3b2d9f763f955afd86ef89b5a52de4465ba10
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2024-06-07 02:26:56 +0100

    Basic mod setup

--- src/client/java/phanastrae/mirthlight_encore/MirthlightEncoreClient.java
+++ src/client/java/phanastrae/mirthlight_encore/MirthlightEncoreClient.java
@@ -3,8 +3,9 @@ package phanastrae.mirthlight_encore;
 import net.fabricmc.api.ClientModInitializer;
 
 public class MirthlightEncoreClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
 	}
 }
