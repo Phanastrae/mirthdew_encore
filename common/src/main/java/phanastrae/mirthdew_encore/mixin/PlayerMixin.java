5e3f0eecef6c83e92d307f7eee696d60310513af
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2025-03-14 23:10:00 +0000

    fix neoforge being neoforge (this is the real 0.2.0)

--- common/src/main/java/phanastrae/mirthdew_encore/mixin/PlayerMixin.java
+++ common/src/main/java/phanastrae/mirthdew_encore/mixin/PlayerMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import phanastrae.mirthdew_encore.duck.PlayerDuckInterface;
 import phanastrae.mirthdew_encore.entity.MirthdewEncorePlayerEntityAttachment;
-import phanastrae.mirthdew_encore.registry.MirthdewEncoreFluidTags;
 
 import static phanastrae.mirthdew_encore.entity.effect.MirthdewEncoreStatusEffects.DREAMY_DIET_ENTRY;
 
@@ -65,15 +63,6 @@ public abstract class PlayerMixin extends LivingEntity implements PlayerDuckInte
     private void mirthdew_encore$canAlwaysConsumeIfDreamyDieting(boolean ignoreHunger, CallbackInfoReturnable<Boolean> cir) {
         if (this.hasEffect(DREAMY_DIET_ENTRY)) {
             cir.setReturnValue(true);
-        }
-    }
-
-    @Inject(method = "playStepSound", at = @At("HEAD"), cancellable = true)
-    private void mirthdew_encore$fluidStepSounds(BlockPos pos, BlockState state, CallbackInfo ci) {
-        if(this.getFluidHeight(MirthdewEncoreFluidTags.VESPERBILE) > 0.0) {
-            this.waterSwimSound();
-            this.playMuffledStepSound(state);
-            ci.cancel();
         }
     }
 
