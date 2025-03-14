241d8c4619c6443a0bde287c5915789c87e30a09
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2025-03-14 22:48:35 +0000

    Fixed Warping kicking players for Flying

--- common/src/main/java/phanastrae/mirthdew_encore/mixin/ServerGamePacketListenerImplMixin.java
+++ common/src/main/java/phanastrae/mirthdew_encore/mixin/ServerGamePacketListenerImplMixin.java
@@ -11,16 +11,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import phanastrae.mirthdew_encore.dreamtwirl.DreamtwirlLevelAttachment;
+import phanastrae.mirthdew_encore.entity.MirthdewEncoreEntityAttachment;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImplMixin {
 
     @Shadow public ServerPlayer player;
 
+    @Shadow private boolean clientIsFloating;
+
+    @Shadow private int aboveGroundTickCount;
+
     @Inject(method = "handleInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/border/WorldBorder;isWithinBounds(Lnet/minecraft/core/BlockPos;)Z"), cancellable = true)
     private void mirthdew_encore$cancelEntityInteraction(ServerboundInteractPacket packet, CallbackInfo ci, @Local(ordinal = 0) Entity entity) {
         if(DreamtwirlLevelAttachment.positionsAreInSeparateDreamtwirls(this.player.level(), this.player.position(), entity.position())) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void mirthdew_encore$preventWarpingCountingAsFlying(CallbackInfo ci) {
+        if(MirthdewEncoreEntityAttachment.fromEntity(this.player).isWarping()) {
+            this.clientIsFloating = false;
+            this.aboveGroundTickCount = 0;
         }
     }
 }
